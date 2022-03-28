dedda9d1c1f28fada79d3c2631ae56acdee55a21
Chan Choi <chanchoi829@gmail.com>
2022-03-28 20:42:28 +0900

    Fix anchor test for solpowerball

--- programs/solpowerball/src/lib.rs
+++ programs/solpowerball/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8rdLvCDitZ7CMXAWZpVBr3pjxXY5Y46acSRzReBP5VMp");
 
 #[program]
 pub mod solpowerball {
